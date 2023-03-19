cfb45b8166fdfacfec26a35a15c1824c890170ac
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-19 23:10:01 +0530

    complete Data Type question on GFG

---
+++ Step 1 - Learn the basics/Step 1.1 - Things to Know in C++/Java/Python or any language/Data Type - GFG.py
@@ -0,0 +1,48 @@
+# ---------------- MSB's Coding Template ---------------- #
+'''
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+'''
+
+# ---I/O from file---#
+import sys
+try:
+    sys.stdin = open("input.txt", "r")
+    sys.stdout = open("output.txt", "w")
+    sys.stderr = open("error.txt", "w")
+except:
+    pass
+
+
+# ---------------------- Code Starts Here ----------------------#
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+# } Driver Code Ends
+# User function Template for python3
+
+class Solution:
+    def dataTypeSize(self, str):
+        if str == "Character":
+            return 1
+        elif str == "Integer":
+            return 4
+        elif str == "Long":
+            return 8
+        elif str == "Float":
+            return 4
+        else:
+            return 8
+
+
+# {
+# Driver Code Starts.
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        str = input()
+        ob = Solution()
+        res = ob.dataTypeSize(str)
+        print(res)
+# } Driver Code Ends
