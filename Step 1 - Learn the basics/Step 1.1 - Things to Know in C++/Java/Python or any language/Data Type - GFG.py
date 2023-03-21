81c6b9d6b9066782cf415e9a9ad410f3bc663adb
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-21 23:22:02 +0530

    add the link to the question

--- Step 1 - Learn the basics/Step 1.1 - Things to Know in C++/Java/Python or any language/Data Type - GFG.py
+++ Step 1 - Learn the basics/Step 1.1 - Things to Know in C++/Java/Python or any language/Data Type - GFG.py
@@ -12,6 +12,7 @@ try:
 except:
     pass
 
+# link - https://practice.geeksforgeeks.org/problems/data-type-1666706751/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=data-type
 
 # ---------------------- Code Starts Here ----------------------#
 
