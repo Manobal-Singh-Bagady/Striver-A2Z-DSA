466e1387e351c619cbb2273fe5d3d354cc2b0f09
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-03-29 01:23:55 +0530

    add link to the question

--- Step 1 - Learn the basics/Step 1.2 - Build-up Logical Thinking/Pattern 2.py
+++ Step 1 - Learn the basics/Step 1.2 - Build-up Logical Thinking/Pattern 2.py
@@ -12,6 +12,7 @@ try:
 except:
     pass
 
+# Link - https://practice.geeksforgeeks.org/problems/right-triangle/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_2
 
 # ---------------------- Code Starts Here ----------------------#
 
