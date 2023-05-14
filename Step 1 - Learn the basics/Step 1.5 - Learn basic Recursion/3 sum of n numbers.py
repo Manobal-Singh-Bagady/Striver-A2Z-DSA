b8ceec8d4adf003e8914605bc8753542bf2b3369
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-05-15 01:25:41 +0530

    :recycle: 1.5 sum of first n numbers

---
+++ Step 1 - Learn the basics/Step 1.5 - Learn basic Recursion/3 sum of n numbers.py
@@ -0,0 +1,24 @@
+# ---------------- MSB's Coding Template ---------------- #
+"""
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+"""
+# ---I/O from file---#
+import sys
+
+try:
+    sys.stdin = open("input.txt", "r", encoding="UTF-8")
+    sys.stdout = open("output.txt", "w", encoding="UTF-8")
+    sys.stderr = open("output.txt", "w", encoding="UTF-8")
+except FileNotFoundError as __:
+    pass
+
+
+# ---------------------- Code Starts Here ----------------------#
+def sumofN(n):
+    if n == 0:
+        return 0
+    return n + sumofN(n - 1)
+
+
+if __name__ == "__main__":
+    print(sumofN(int(input())))
