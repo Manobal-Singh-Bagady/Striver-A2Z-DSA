20856272eeb3e3de083d22b66166be8d76971f59
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-05-24 01:57:46 +0530

    :recycle: 1.5 print name N times

---
+++ Step 1 - Learn the basics/Step 1.5 - Learn basic Recursion/1 Print name N times.py
@@ -0,0 +1,41 @@
+# ---------------- MSB's Coding Template ---------------- #
+"""
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+"""
+
+# ---I/O from file---#
+import sys
+
+try:
+    sys.stdin = open("input.txt", "r", encoding="UTF-8")
+    sys.stdout = open("output.txt", "w", encoding="UTF-8")
+    sys.stderr = open("output.txt", "w", encoding="UTF-8")
+except FileNotFoundError as __:
+    pass
+
+
+# ---------------------- Code Starts Here ----------------------#
+
+# User function Template for python3
+
+
+class Solution:
+    def printGfg(self, n):
+        if n == 0:
+            return 0
+        print("GFG", end=" ")
+        self.printGfg(n - 1)
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printGfg(N)
+        print()
+# } Driver Code Ends
