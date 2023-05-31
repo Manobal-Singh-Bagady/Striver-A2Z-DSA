1bd3dfe2e8fe72d54dd305c95fc560fcbd42c55f
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-05-31 13:31:44 +0530

    :white_check_mark: 1.5 all factorial less than N - gfg

---
+++ Step 1 - Learn the basics/Step 1.5 - Learn basic Recursion/5 all factorial less than N - gfg.py
@@ -0,0 +1,50 @@
+# ---------------- MSB's Coding Template ---------------- #
+"""
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+"""
+
+# ---I/O from file---#
+import sys
+
+try:
+    sys.stdin = open("input.txt", "r", encoding="UTF-8")
+    sys.stdout = open("output.txt", "w", encoding="UTF-8")
+    sys.stderr = open("output.txt", "w", encoding="UTF-8")
+except FileNotFoundError as __:
+    pass
+
+
+# ---------------------- Code Starts Here ----------------------#
+# User function Template for python3
+
+
+class Solution:
+    def factorialNumbers(self, N):
+        ans = []
+
+        def fact(n, fac, i):
+            i += 1
+            if fac > n:
+                return
+            ans.append(fac)
+            fact(n, fac * i, i)
+
+        fact(N, 1, 1)
+        return ans
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.factorialNumbers(N)
+        for i in ans:
+            print(i, end=" ")
+        print()
+
+# } Driver Code Ends
