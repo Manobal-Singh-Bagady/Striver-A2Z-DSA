4f336cdfa06dc9c0c3332c9b008d932377ba42cb
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-06-06 00:45:01 +0530

    :white_check_mark: 1.6 - highest and lowest frequency in an array
    Step 1.6 - Learn basic Hashing.
    
    Using the hash map, learn how to find the highest and lowest frequency elements in an array.

---
+++ Step 1 - Learn the basics/Step 1.6 - Learn basic Hashing/2 highest and lowest frequency elements.py
@@ -0,0 +1,34 @@
+# ---------------- MSB's Coding Template ---------------- #
+"""
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+"""
+
+# ---I/O from file---#
+import sys
+
+try:
+    sys.stdin = open("input.txt", "r", encoding="UTF-8")
+    sys.stdout = open("output.txt", "w", encoding="UTF-8")
+    sys.stderr = open("output.txt", "w", encoding="UTF-8")
+except FileNotFoundError as __:
+    pass
+
+
+# ---------------------- Code Starts Here ----------------------#
+
+def highandlowfreq(arr):
+    hashmap = {}
+    for i in arr:
+        if i in hashmap:
+            hashmap[i] += 1
+        else:
+            hashmap[i] = 1
+    return (
+        min(hashmap, key=lambda x: hashmap[x]),
+        max(hashmap, key=lambda x: hashmap[x]),
+    )
+
+
+if __name__ == "__main__":
+    arr = list(map(int, input().split()))
+    print(highandlowfreq(arr))
