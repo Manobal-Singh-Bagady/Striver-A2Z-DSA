b036d302e44410c6a62e751705bb22f240794be4
Manobal Singh Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2023-06-07 16:55:34 +0530

    :white_check_mark: 2.2 - solve bubble sort recursively - gfg
    Step 2.2 - Sorting II.
    
    Solve the Bubble Sort problem on GFG using recursion.

---
+++ Step 2 - Learn Important Sorting Techniques/Step 2.2 - Sorting II/recursive bubble sort - gfg.py
@@ -0,0 +1,51 @@
+# ---------------- MSB's Coding Template ---------------- #
+"""
+    "ɴᴏ ᴄᴏᴅᴇ ɪꜱ ᴘᴇʀꜰᴇᴄᴛ"
+"""
+
+# ---I/O from file---#
+import sys
+
+try:
+    sys.stdin = open("input.txt", "r", encoding="UTF-8")
+    sys.stdout = open("output.txt", "w", encoding="UTF-8")
+    sys.stderr = open("output.txt", "w", encoding="UTF-8")
+except FileNotFoundError as __:
+    pass
+
+
+# ---------------------- Code Starts Here ----------------------#
+
+# User function Template for python3
+
+
+class Solution:
+    # Function to sort the array using bubble sort algorithm.
+    def bubbleSort(self, arr, n):
+        if n == 1:
+            return
+
+        for j in range(n - 1):
+            if arr[j + 1] < arr[j]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+        self.bubbleSort(arr, n - 1)
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ob.bubbleSort(arr, n)
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+# } Driver Code Ends
